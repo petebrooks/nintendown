8099676e7e2905862c3ac950575d6bff0ec9e40a
Pete Brooks <pete@txidigital.com>
2023-11-18 18:19:06 -0600

    Accept yt linx

--- nintendown.py
+++ nintendown.py
@@ -7,36 +7,52 @@ import scdl
 import time
 import os
 
+
 def validate_url(url):
-    regex = re.compile(r"^(https?://)?(www\.)?soundcloud\.com/[\w\-/]+$")
-    return regex.match(url)
+    # Check if it's a SoundCloud URL
+    soundcloud_regex = re.compile(r"^(https?://)?(www\.)?soundcloud\.com/[\w\-/]+$")
+    youtube_regex = re.compile(
+        r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/[\w\-?=&/]+$"
+    )
+    if soundcloud_regex.match(url):
+        return "soundcloud"
+    elif youtube_regex.match(url):
+        return "youtube"
+    return None
 
 
+# TODO: Ask about audio format
 def on_button_click():
     url = pyperclip.paste()
-    if url and validate_url(url):
-        download_dir = filedialog.askdirectory(title='Select Download Directory')
+    url_type = validate_url(url)
+    if url and url_type:
+        download_dir = filedialog.askdirectory(title="Select Download Directory")
         if download_dir:
             os.chdir(download_dir)
-            command = f'scdl -c -l {url}'
-            print(f'Command: {command}')
+            if url_type == "soundcloud":
+                command = f"scdl -c -l {url}"
+            else:  # url_type == "youtube"
+                command = f"youtube-dl -x --audio-format aac {url}"
+            print(f"Command: {command}")
             start_time = time.time()
-            print(f'Start time: {time.ctime(start_time)}')
+            print(f"Start time: {time.ctime(start_time)}")
             try:
-                subprocess.run(command, shell=True, check=True, stderr=subprocess.PIPE, text=True)
+                subprocess.run(
+                    command, shell=True, check=True, stderr=subprocess.PIPE, text=True
+                )
                 messagebox.showinfo("Success", "Download completed successfully")
             except subprocess.CalledProcessError as e:
                 messagebox.showerror("Error", e.stderr)
             end_time = time.time()
             elapsed_time = end_time - start_time
-            print(f'End time: {time.ctime(end_time)}')
-            print(f'Time elapsed: {elapsed_time} seconds')
+            print(f"End time: {time.ctime(end_time)}")
+            print(f"Time elapsed: {elapsed_time} seconds")
         else:
             messagebox.showinfo("Info", "Download cancelled")
     elif not url:
         messagebox.showerror("Error", "No URL found in clipboard")
     else:
-        messagebox.showerror("Error", "Invalid SoundCloud URL")
+        messagebox.showerror("Error", "Invalid URL")
 
 
 # Create the main window
