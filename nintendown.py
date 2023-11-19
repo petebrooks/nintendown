9c3ccefe3c0c5c58873b7ceb7cd0b66c517c0b11
Pete Brooks <pete@txidigital.com>
2023-11-18 18:37:12 -0600

    less long method

--- nintendown.py
+++ nintendown.py
@@ -9,7 +9,6 @@ import os
 
 
 def validate_url(url):
-    # Check if it's a SoundCloud URL
     soundcloud_regex = re.compile(r"^(https?://)?(www\.)?soundcloud\.com/[\w\-/]+$")
     youtube_regex = re.compile(
         r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/[\w\-?=&/]+$"
@@ -21,7 +20,27 @@ def validate_url(url):
     return None
 
 
-# TODO: Ask about audio format
+def download_audio(url, url_type, download_dir):
+    if url_type == "soundcloud":
+        command = f"scdl -c -l {url}"
+    else:  # url_type == "youtube"
+        command = f"youtube-dl -x --audio-format mp3 {url}"
+
+    print(f"Command: {command}")
+    start_time = time.time()
+    print(f"Start time: {time.ctime(start_time)}")
+    try:
+        subprocess.run(
+            command, shell=True, check=True, stderr=subprocess.PIPE, text=True
+        )
+        messagebox.showinfo("Success", "Download completed successfully")
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", e.stderr)
+    end_time = time.time()
+    print(f"End time: {time.ctime(end_time)}")
+    print(f"Time elapsed: {elapsed_time} seconds")
+
+
 def on_button_click():
     url = pyperclip.paste()
     url_type = validate_url(url)
@@ -29,24 +48,7 @@ def on_button_click():
         download_dir = filedialog.askdirectory(title="Select Download Directory")
         if download_dir:
             os.chdir(download_dir)
-            if url_type == "soundcloud":
-                command = f"scdl -c -l {url}"
-            else:  # url_type == "youtube"
-                command = f"youtube-dl -x --audio-format aac {url}"
-            print(f"Command: {command}")
-            start_time = time.time()
-            print(f"Start time: {time.ctime(start_time)}")
-            try:
-                subprocess.run(
-                    command, shell=True, check=True, stderr=subprocess.PIPE, text=True
-                )
-                messagebox.showinfo("Success", "Download completed successfully")
-            except subprocess.CalledProcessError as e:
-                messagebox.showerror("Error", e.stderr)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print(f"End time: {time.ctime(end_time)}")
-            print(f"Time elapsed: {elapsed_time} seconds")
+            download_audio(url, url_type, download_dir)
         else:
             messagebox.showinfo("Info", "Download cancelled")
     elif not url:
